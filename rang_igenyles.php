fd5580e3439b82eb191155f3e08e0848c7d6f75b
nbela12 <131850913+nbela12@users.noreply.github.com>
2023-04-29 11:24:33 +0200

    Update rang_igenyles.php

--- rang_igenyles.php
+++ rang_igenyles.php
@@ -1,26 +1,27 @@
 <?php
 session_start();
-request_once (" sql_conf.php ");
+require_once("sql_conf.php");
 
-$ nev = $ _POST [" igenyles_nev "];
-$ rendfokozat = $ _POST [" igenyles_rendfokozat "];
-$ alosztaly = $ _POST [" igenyles_alosztaly "];
-$ uj_rendfokozat = $ _POST [" igenyles_jrendfokozat "];
-$ cel = $ _POST [" Fogalmazza meg 1-2 mondatban az előléptetése célját "];
-$ miota = $ _POST [" igenyles_miota "];
+$nev = $_POST["igenyles_nev"];
+$rendfokozat = $_POST["igenyles_rendfokozat"];
+$alosztaly = $_POST["igenyles_alosztaly"];
+$uj_rendfokozat = $_POST["igenyles_jrendfokozat"];
+$cel = $_POST["Fogalmazza meg 1-2 mondatban az előléptetése célját"];
+$miota = $_POST["igenyles_miota"];
 
-if (!isset( $ _SESSION [" Név "]) && $ _SESSION [" Naplózott "] == false )
+if(!isset($_SESSION["Name"]) && $_SESSION["Logged"] == false)
 {
-	visszhang "
+	echo "
 	
-	<audio automatikus lejátszása>
+	<audio autoplay>
+
 	<source src='../sorry.mp3' type='audio/mpeg'>
 	
 	</audio>
 	
 	MENJÉ INNÉ #yolo<br>
 	De ha ezt elnem olvasod akkor is menjé inné!<br>
-	rendben?<br>
+	oké?<br>
 	Na menjé<br>
 	Várlak<br>
 	MENJÉ MÁR INNÉ<br>
@@ -29,7 +30,7 @@ if (!isset( $ _SESSION [" Név "]) && $ _SESSION [" Naplózott "] == false )
 	
 	";
 }
-más
+else
 {
 	
 }
